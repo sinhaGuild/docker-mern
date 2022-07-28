eb63219fe8e09d135ac3484c909459d5d2185a26
Baroque House <concierge@thebaroquehouse.com>
2022-07-28 01:09:13 -0400

    PROD complete.

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,44 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import "./darkly.css";
 
 function App() {
   const [posts, setPosts] = useState([]);
-  const uri = "http://localhost:3050/api/posts";
+  const postsEndpoint = "/api/posts";
 
   useEffect(() => {
-    fetch(uri)
-      .then((resp) => resp.json())
-      .then((data) => {
-        console.log(
-          `Data Fetch sucessful. Data: ${JSON.stringify(data, null, 2)}`
-        );
-        setPosts(data);
-      });
+    getPosts();
   }, []);
 
+  const getPosts = async () => {
+    const response = await fetch(postsEndpoint);
+    const dataResponse = await response.json();
+    const {
+      data: { posts: _data },
+    } = dataResponse;
+    setPosts(_data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>All Posts</h1>
-      </header>
-      <div className="posts-container">
-        {posts.length > 0 &&
-          posts.map((post) => {
-            return (
-              <>
-                <pre>{JSON.stringify(post, null, 2)}</pre>
-              </>
-            );
-          })}
+    <div className="container">
+      <h1 className="mb-2 mt-2 display-1">All Posts in DB</h1>
+      <div className="row-cols-3">
+        {posts.map((post) => {
+          return (
+            <>
+              <div className="card mt-2 mb-2 border-primary">
+                <div className="card-header">
+                  <h4>{post.title}</h4>
+                </div>
+                <div className="card-body">
+                  <div className="card-text">
+                    <p>{post.body}</p>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
       </div>
     </div>
   );
